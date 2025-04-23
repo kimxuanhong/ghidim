9604337944a6f1bfbeb62cf1da2800939bb09491
kimxuanhong <40137730+kimxuanhong@users.noreply.github.com>
2025-04-23 07:35:56 +0000

    Deploying to gh-pages3 from @ kimxuanhong/ghidim@e65637be466eabe2bc2524d0b9beb577dda14af0 🚀

---
+++ sw-register.js
@@ -0,0 +1,6 @@
+<!-- Service Worker Registration  -->
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('Service Worker registered with scope:', reg.scope))
+        .catch(err => console.error('Service Worker registration failed:', err));
+}
